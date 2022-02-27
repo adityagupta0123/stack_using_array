cbdb5cd96267ee504a373db9e12634480d292426
adityagupta0123 <aditya.gupta3182003@gmail.com>
2022-02-27 15:07:14 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+class Stack {
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack
+
+    Stack()
+    {
+        top = -1;
+    }
+    boolean isEmpty()
+    {
+        return (top < 0);
+    }
+
+    void push(int x)
+    {
+        if (top >= (MAX - 1)) {
+            System.out.println("Stack Overflow");
+//            return false;
+        }
+        else {
+            a[++top] = x;
+            System.out.println(x + " pushed into stack");
+//            return true;
+        }
+    }
+
+    int pop()
+    {
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else {
+            int x = a[top--];
+            return x;
+        }
+    }
+
+    int peek()
+    {
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else {
+            int x = a[top];
+            return x;
+        }
+    }
+
+    void print(){
+        for(int i = top;i>-1;i--){
+            System.out.print(" "+ a[i]);
+        }
+    }
+}
+
+// Driver code
+class Main {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        Stack s = new Stack();
+        System.out.println("enter data to push ");
+        s.push(sc.nextInt());
+        s.push(sc.nextInt());
+        s.push(sc.nextInt());
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println("Top element is :" + s.peek());
+        System.out.print("Elements present in stack :");
+        s.print();
+    }
+}
